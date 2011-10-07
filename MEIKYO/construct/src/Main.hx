c9a7bd2367e11d4b28a5c2ff4e45c8491b3eebb0
Nico Zimmermann <hello@britzpetermann.com>
2011-10-07 12:43:35 +0200

    added libs

--- MEIKYO/construct/src/Main.hx
+++ MEIKYO/construct/src/Main.hx
@@ -1,7 +1,30 @@
 class Main {
 
-	public static function main() {
-		
+	static function globalErrorHandler(msg : String, stack : Array<String>)
+	{
+		trace("Uncaugt error: " + msg);
+		for(line in stack)
+			trace(line);
+
+		return true;
+	}
+
+	static function main()
+	{
+		bpmjs.LogContext.setRedirection();
+		js.Lib.setErrorHandler(globalErrorHandler);
+	}
+
+	function new(canvas : Canvas)
+	{
+		try
+		{
+			var context = bpmjs.ContextBuilder.build(Config);
+		}
+		catch(e : Dynamic)
+		{
+			trace("Error building application: " + e);
+		}
 	}
 
 }
